3bbd34b01273caf2ab4fd38f1883dc1ec55b7d16
AnkushRawat <ankushrwt344@gmail.com>
2022-05-06 20:45:13 +0530

    Add project files.

---
+++ DelegateDemo/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DelegateDemo;
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Delegate Demo");
+//operation obj = new operation(Addition);
+//Console.WriteLine("Addition is = {0}",obj(23,27));
+MulticastDelegate1.ImplementDelegate();
+Console.ReadKey();
---
+++ EventsDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EventsDemo;
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello World!");
+//add += new DelEventHandler(India);
+//add += new DelEventHandler(USA);
+//add += new DelEventHandler(England);
+//add.Invoke();
+Console.WriteLine("************Event Handling****************");
+EventHandling.ImplementEvent();
+Console.ReadKey();
